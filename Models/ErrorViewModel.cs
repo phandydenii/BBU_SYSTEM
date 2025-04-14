7a4af83609b14d7779c4e0844d5876db2d2d7576
phandydenii <phandy010@gmail.com>
2025-04-14 11:20:59 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BBU_SYSTEM.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
